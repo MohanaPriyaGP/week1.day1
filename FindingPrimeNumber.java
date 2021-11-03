f3fcaec7d54327792dcf7a7f835cb10def3de2b6
MohanaPriyaGP <93474279+MohanaPriyaGP@users.noreply.github.com>
2021-11-04 02:18:50 +0530

    Add files via upload

---
+++ FindingPrimeNumber.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class FindingPrimeNumber {
+
+	public static void main(String[] args) {
+		int input = 17;
+		//boolean bol = false;
+		
+		for(int i=0;i<=input;i++)
+			
+		{
+			if(i%2!=0)
+			{
+				System.out.print(i  +" is prime "+",");
+			}
+		
+			else
+			
+				System.out.print(i  +" is even "+",");
+			
+		
+			
+		}
+				
+		}
+}
+			
+		
+		
+
+	
